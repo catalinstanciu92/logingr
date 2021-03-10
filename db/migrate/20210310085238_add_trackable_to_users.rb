0c9d75c785f9e546563cd53143271d85e761ccfc
Catalin Stanciu <catalin.staciu@prificircle.ro>
2021-03-10 11:01:55 +0200

    added activated_at and trackable

---
+++ db/migrate/20210310085238_add_trackable_to_users.rb
@@ -0,0 +1,14 @@
+class AddTrackableToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :activation_digest, :string
+    add_column :users, :activated, :boolean, default: false
+    add_column :users, :activated_at, :datetime
+    ## Trackable
+    add_column :users, :sign_in_count, :integer, default: 0, null: false
+    add_column :users, :current_sign_in_at, :datetime
+    add_column :users, :last_sign_in_at, :datetime
+    add_column :users, :current_sign_in_ip, :string
+    add_column :users, :last_sign_in_ip, :string
+
+  end
+end
